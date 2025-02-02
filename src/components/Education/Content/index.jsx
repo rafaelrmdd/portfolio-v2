0347f02f818e18d7f6cd9aba4b0f87287d6b565f
Rafael Rodrigues <rafaelrodriguesmirandadiniz@gmail.com>
2025-02-02 13:18:51 -0300

    add: degree section in education page

--- src/components/Education/Content/index.jsx
+++ src/components/Education/Content/index.jsx
@@ -1,5 +1,6 @@
 import Header from "@/components/Header";
 import GraduationImage from "@/assets/education-image.png"
+import UnipLogoImage from "@/assets/unip-logo.png"
 import Image from "next/image";
 
 export default function Content() {
@@ -7,7 +8,7 @@ export default function Content() {
         <div className="px-36 bg-cyan-50 w-full h-full">
             <Header />
 
-            <main className="mt-16">
+            <main className="mt-16 mb-16">
                 <div className="flex justify-between h-[500px]">
                     <div className="w-[50%] ">
                         <Image src={GraduationImage} className="object-fill  w-4/5"/>
@@ -22,13 +23,22 @@ export default function Content() {
                     <h2 className="text-5xl text-center">Minha Formação</h2>
 
                     <div className="flex mt-16">
+
                         {/* University Logo */}
-                        <div className="w-[20%]">
-                            Logo
+                        <div className="w-[20%] flex flex-col gap-y-2 justify-center items-center">
+                            <Image src={UnipLogoImage} className="w-24 h-16"/>
+
+                            <div>
+                                <span className="font-semibold">2022 - 2024</span>
+                            </div>
                         </div>
 
                         <div className="w-[80%]">
-                            aaaaa
+                            <h2 className="text-2xl">Análise e Desenvolvimento de Sistemas</h2>
+                            <h2 className="text-lg">Universidade Paulista (UNIP)</h2>
+                            <h3 className="text-lg">● Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta, excepturi.</h3>
+                            <h3 className="text-lg">● Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta, excepturi.</h3>
+                            <h3 className="text-lg">● Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta, excepturi.</h3>
                         </div>
                     </div>
                 </div>
