44af599b092cee753b543ccad550cf4aec79f738
Rafael Rodrigues <rafaelrodriguesmirandadiniz@gmail.com>
2025-02-12 14:42:13 -0300

    change: educationpage font color

--- src/components/Education/DegreeItem/index.jsx
+++ src/components/Education/DegreeItem/index.jsx
@@ -24,17 +24,17 @@ export default function DegreeItem() {
                 <h2 className="text-2xl text-indigo-800">Análise e Desenvolvimento de Sistemas</h2>
 
                 <h2 className="text-lg text-indigo-800">Universidade Paulista (UNIP)</h2>
-                <h3 className="text-lg text-indigo-700 mt-2">
+                <h3 className="text-lg text-[#7f8daa] mt-2">
                     <span className="text-blue-900">● </span>
                     Graduação em Análise e Desenvolvimento de Sistemas com ênfase em desenvolvimento full-stack e resolução de problemas complexos de software
                 </h3>
 
-                <h3 className="text-lg text-indigo-700">
+                <h3 className="text-lg text-[#7f8daa]">
                     <span className="text-blue-900">● </span>
                     Experiência prática em projetos acadêmicos utilizando tecnologias modernas como React, Node.js e metodologias ágeis Scrum
                 </h3>
 
-                <h3 className="text-lg text-indigo-700">
+                <h3 className="text-lg text-[#7f8daa]">
                     <span className="text-blue-900">● </span>
                     Domínio em desenvolvimento de sistemas web, mobile e cloud, com foco em criar soluções inovadoras e eficientes para desafios tecnológicos
                 </h3>
